ff95e8fc3da54b33446ea098fcea76d6d52cb0d1
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-12 21:01:53 -0300

    block class

---
+++ Blockchain.Application/Services/WalletService.cs
@@ -0,0 +1,12 @@
+﻿using Blockchain.Domain.Services;
+
+namespace Blockchain.Application.Services
+{
+    public class WalletService : IWalletService
+    {
+        public void SendMoney(double amount, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blockchain.Engine/Block.cs
@@ -0,0 +1,30 @@
+﻿namespace Blockchain.Engine
+{
+    public class Block
+    {
+        public Header Header { get; set; }
+        public Payload Payload { get; set; }
+    }
+
+    public class Header
+    {
+        public int Nonce { get; set; }
+        public string BlockHash { get; set; }
+    }
+
+    public class Payload
+    {
+        public int Sequence { get; set; }
+        public int Timesetamp { get; set; }
+        public Data Data { get; set; }
+        public string PreviousHash { get; set; }
+    }
+
+    public class Data
+    {
+        public Guid From { get; set; }
+        public Guid To { get; set; }
+        public decimal Amout { get; set; }
+    }
+
+}
