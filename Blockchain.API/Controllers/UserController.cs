4177b4eb4bd76af7f3d65cda5420e37b6a211b2f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-16 20:15:32 -0300

    final test

--- Blockchain.API/Controllers/UserController.cs
+++ Blockchain.API/Controllers/UserController.cs
@@ -14,7 +14,7 @@ namespace Blockchain.API.Controllers
         {
             var user = new User(name);
             await service.Create(user);
-            return "test3";
+            return "final test";
         }
     }
 }
